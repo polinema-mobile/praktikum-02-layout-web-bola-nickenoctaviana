202a85e9175235c8f1e38a75b265c149c279535b
= <=>
2021-02-16 11:37:10 +0700

    kode Button

--- lib/main.dart
+++ lib/main.dart
@@ -11,18 +11,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       
-      home: Container (
-        margin: EdgeInsets.only(top:30),
-        color: Colors.white,
-        child: Column(
-          children: <Widget>[
-            AppBar(title: Text('Contoh Cupertino')),
-            CupertinoButton(
-              child: Text("Contoh button"),
-              onPressed: () {},
-              ),
-              CupertinoActivityIndicator()
-          ],
+      home: Scaffold (
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+
+          },
+          child: Icon(Icons.thumb_up),
+          backgroundColor: Colors.pink,
         ),
       ),
     );
