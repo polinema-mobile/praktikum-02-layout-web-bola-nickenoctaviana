952433372188a1eef0d9cde2600bc278771cf4ea
= <=>
2021-02-17 22:00:51 +0700

    Menambahkan judul dan gambar praktikum

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,23 +5,45 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
+  build(context) {
     return MaterialApp(
-      
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Contoh TextField")
-        ),
-        body: TextField(
-          obscureText: false,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            labelText: 'Nama',
-          ),
-        ),
+        home: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red[800],
+        title: Text('MyApp'),
       ),
-    );
+      body: ListView(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              FlatButton(
+                child: Text("BERITA TERBARU"),
+              ),
+              FlatButton(
+                child: Text("PERTANDINGAN HARI INI"),
+              )
+            ],
+          ),
+          Container(
+            child: Image.network(
+                'https://i2-prod.football.london/incoming/article19603985.ece/ALTERNATES/s615/1_GettyImages-1292162465.jpg'),
+          ),
+          Container(
+            height: 40,
+            child: const Center(
+                child: Text(
+              'Costa Mendekat ke Palmeiras',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            )),
+          ),
+          Container(
+            height: 50,
+            color: Colors.purple[300],
+            child: Text('Transfer'),
+          ),
+        ],
+      ),
+    ));
   }
 }
